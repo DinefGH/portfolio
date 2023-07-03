52c2c2389b8ae35ae0acf3dca1c5126cb305be74
DinefGH <fabianduerr@web.de>
2023-07-03 04:47:25 +0200

    added pictures and working on slideshow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,9 +5,13 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
 export class AppComponent implements OnInit  {
   images = ['books-1281581_1920.jpg', 'coding-924920_1920.jpg', 'study-789631_1920.jpg',]
   currentImage = 0;
+  showImage = true;
+
   ngOnInit() {
   this.updateImage();
   }
@@ -17,6 +21,11 @@ export class AppComponent implements OnInit  {
     setInterval(() => {
   this.currentImage++;
   this.currentImage = this.currentImage % this.images.length;
+  this.showImage = false
+
+  setTimeout(() => {
+    this.showImage = true
+  }, 100);
     },8000);
   }
 }
