f3860bb4e03d1f8034731b44d6bb9a4311274fa8
DinefGH <fabianduerr@web.de>
2023-07-03 04:37:37 +0200

    first commit2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'fabianduerr';
+export class AppComponent implements OnInit  {
+  images = ['books-1281581_1920.jpg', 'coding-924920_1920.jpg', 'study-789631_1920.jpg',]
+  currentImage = 0;
+  ngOnInit() {
+  this.updateImage();
+  }
+
+  updateImage() {
+
+    setInterval(() => {
+  this.currentImage++;
+  this.currentImage = this.currentImage % this.images.length;
+    },8000);
+  }
 }
